2d2491df929c0b687829e8e1458ff88825bf72a2
zineblabiad03 <147519431+zineblabiad03@users.noreply.github.com>
2025-11-12 10:23:38 +0100

    init

---
+++ ecommerce/src/test/java/com/ulco/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulco.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
